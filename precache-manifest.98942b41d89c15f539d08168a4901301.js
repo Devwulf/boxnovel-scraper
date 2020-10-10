347103f657a9192108712b9c6c94b13ee25055cd
Mark Malabanan <mark.malabanan24@gmail.com>
2020-10-10 19:21:07 -0400

    Updates

---
+++ precache-manifest.98942b41d89c15f539d08168a4901301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da941a433500476cf10c84a13291ea66",
+    "url": "/boxnovel-scraper/index.html"
+  },
+  {
+    "revision": "f28f2fcdc7e515f43c12",
+    "url": "/boxnovel-scraper/static/css/main.812ab637.chunk.css"
+  },
+  {
+    "revision": "b1d995b67f79dc70a76f",
+    "url": "/boxnovel-scraper/static/js/2.7c36fbd4.chunk.js"
+  },
+  {
+    "revision": "3ade1d5c1f899f8112cc5ef9f37b3e61",
+    "url": "/boxnovel-scraper/static/js/2.7c36fbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f28f2fcdc7e515f43c12",
+    "url": "/boxnovel-scraper/static/js/main.8061f142.chunk.js"
+  },
+  {
+    "revision": "e342b73e27b7740747bf",
+    "url": "/boxnovel-scraper/static/js/runtime-main.2358e537.js"
+  }
+]);
